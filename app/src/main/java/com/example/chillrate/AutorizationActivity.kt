7cadd55a706a42b43cd1b059eadb61586d5cef47
Phoennikxs <phoennikxs@gmail.com>
2026-02-19 15:44:53 +0300

    Добавлен приветственный экран

--- app/src/main/java/com/example/chillrate/AutorizationActivity.kt
+++ app/src/main/java/com/example/chillrate/AutorizationActivity.kt
@@ -2,10 +2,16 @@ package com.example.chillrate
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Layout
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintSet
 
 class AutorizationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +25,20 @@ class AutorizationActivity : AppCompatActivity() {
 
         var AutorizationButton: Button = findViewById(R.id.btn_log_in)
         var GoToRegistrationButton: Button = findViewById(R.id.btn_go_to_reg)
+        var AutriztionLayout = findViewById<View>(R.id.autorization)
+        var WelcomeLayout = findViewById<View>(R.id.welcome_screen)
 
         AutorizationButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
+
+            WelcomeLayout.visibility = View.VISIBLE
+
+            Handler(Looper.getMainLooper()).postDelayed({
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }, 2500)
         }
 
         GoToRegistrationButton.setOnClickListener {
@@ -34,5 +49,4 @@ class AutorizationActivity : AppCompatActivity() {
             }
         }
     }
-
 }
